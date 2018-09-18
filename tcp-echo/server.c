46bd0395173b1ec3d4af135578a5a602b7d1ffbc
Fan Wu <386534960@qq.com>
2018-09-18 22:46:16 +0800

    修复server的问题，避免内存泄漏

--- tcp-echo/server.c
+++ tcp-echo/server.c
@@ -1,4 +1,5 @@
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <uv.h>
 
@@ -6,61 +7,71 @@
 #define DEFAULT_BACKLOG 16
 uv_loop_t *loop;
 
-void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
+typedef struct write_req {
+  uv_write_t req;
+  uv_buf_t buf;
+} write_req_t;
+
+void free_write_req(uv_write_t *req) {
+  write_req_t *wr = (write_req_t *)req;
+  free(wr->buf.base);
+  free(wr);
+}
+
+void on_alloc(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf) {
   buf->base = (char *)malloc(suggested_size);
   buf->len = suggested_size;
 }
 
-void echo_write(uv_write_t *req, int status) {
+void on_write(uv_write_t *req, int status) {
   if (status != 0) {
-    fprintf(stderr, "Write error %s\n", uv_strerror(status));
+    fprintf(stderr, "write failed: %s\n", uv_strerror(status));
   }
-  free(req);
+  free_write_req(req);
 }
 
-void echo_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf) {
+void on_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf) {
+  if (nread > 0) {
+    write_req_t *req = (write_req_t *)malloc(sizeof(write_req_t));
+    req->buf = uv_buf_init(buf->base, nread);
+    uv_write((uv_write_t *)req, client, &req->buf, 1, on_write);
+    return;
+  }
   if (nread < 0) {
     if (nread != UV_EOF) {
-      fprintf(stderr, "Read error %s\n", uv_err_name(nread));
-      uv_close((uv_handle_t *)client, NULL);
+      fprintf(stderr, "Read failed: %s\n", uv_strerror(nread));
     }
-  } else if (nread > 0) {
-    uv_write_t *req = (uv_write_t *)malloc(sizeof(uv_write_t));
-    uv_buf_t wrbuf = uv_buf_init(buf->base, nread);
-    uv_write(req, client, &wrbuf, 1, echo_write);
+    uv_close((uv_handle_t *)client, NULL);
   }
-  if (buf->base) {
-    free(buf->base);
-  }
+  free(buf->base);
 }
 
 void on_new_connection(uv_stream_t *server, int status) {
-  if (status < 0) {
-    fprintf(stderr, "New connection error %s\n", uv_strerror(status));
+  if (status != 0) {
+    fprintf(stderr, "new connection failed: %s\n", uv_strerror(status));
     return;
   }
-
   uv_tcp_t *client = (uv_tcp_t *)malloc(sizeof(uv_tcp_t));
   uv_tcp_init(loop, client);
-  if (uv_accept(server, (uv_stream_t *)client) != 0) {
-    uv_close((uv_handle_t *)client, NULL);
+  if (uv_accept(server, (uv_stream_t *)client) == 0) {
+    uv_read_start((uv_stream_t *)client, on_alloc, on_read);
   } else {
-    uv_read_start((uv_stream_t *)client, alloc_buffer, echo_read);
+    uv_close((uv_handle_t *)client, NULL);
   }
 }
 
 int main() {
   loop = uv_default_loop();
-  uv_tcp_t server;
   struct sockaddr_in addr;
+  uv_tcp_t server;
 
   uv_tcp_init(loop, &server);
   uv_ip4_addr("0.0.0.0", DEFAULT_PORT, &addr);
-
   uv_tcp_bind(&server, (const struct sockaddr *)&addr, 0);
+
   int r = uv_listen((uv_stream_t *)&server, DEFAULT_BACKLOG, on_new_connection);
   if (r != 0) {
-    fprintf(stderr, "Listen error: %s\n", uv_strerror(r));
+    fprintf(stderr, "uv_listen failed: %s\n", uv_strerror(r));
     return 1;
   }
   return uv_run(loop, UV_RUN_DEFAULT);
