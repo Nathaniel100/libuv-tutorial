a3d1ca9bcbb2be6b0211f0efc7e785cf3d56ebda
吴凡 <>
2018-09-19 09:05:54 +0800

    更新hello-world代码

--- hello-world/main.c
+++ hello-world/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <uv.h>
 
-int main(int argc, char**argv) {
-  uv_loop_t *loop = uv_default_loop();
+int main(int argc, char **argv) {
+  uv_loop_t *loop = (uv_loop_t *)malloc(sizeof(uv_loop_t));
   uv_loop_init(loop);
   printf("Hello, world!\n");
   uv_run(loop, UV_RUN_DEFAULT);
   uv_loop_close(loop);
+  free(loop);
   return 0;
 }
